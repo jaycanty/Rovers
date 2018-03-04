1584d5d49e5e6cfa4f969f6fe048e7ea8391b6df
Jay Canty <jay.canty@web.com>
2018-03-04 13:31:19 -0500

    Add grid input logic

--- Rovers/SelectGridViewController.swift
+++ Rovers/SelectGridViewController.swift
@@ -12,16 +12,14 @@ class SelectGridViewController: UIViewController {
     
     @IBOutlet weak var gridSizeTextField: UITextField!
     @IBOutlet weak var gridSizeDisplayLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
@@ -34,11 +32,24 @@ class SelectGridViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+    }
 }
 
 extension SelectGridViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let input = ((textField.text as NSString?) ?? "")
+            .replacingCharacters(in: range, with: string)
+        let gridSize = Int(input) ?? 0
+        if (gridSize > 4 && gridSize < 40) {
+            self.gridSizeDisplayLabel.text = gridSize == 0 ? "" : "\(input) X \(input)"
+            self.nextButton.alpha = 1
+        } else {
+            self.gridSizeDisplayLabel.text = "Grid size range: 4-40"
+            self.nextButton.alpha = 0
+        }
         return true
     }
 }
